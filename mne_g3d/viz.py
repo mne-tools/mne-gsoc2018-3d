5eecd7f920dea4b2b44c71df797c1c44e08aabdc
Oleh Kozynets <ok7mailbox@gmail.com>
2018-05-17 15:03:48 +0200

    Options for figire size, azimuth, elevation were added.

--- mne_g3d/viz.py
+++ mne_g3d/viz.py
@@ -1,6 +1,7 @@
 import logging
 
 import ipyvolume as ipv
+from ipyvolume.pylab import style
 import nibabel
 import numpy as np
 
@@ -27,8 +28,78 @@ def read_brain_mesh(surface_path: str):
     return vertices, faces
 
 
-def plot_brain_mesh(vertices, faces,  color='grey'):
+def plot_brain_mesh(rh_vertices=None,
+                    lh_vertices=None,
+                    rh_faces=None,
+                    lh_faces=None,
+                    rh_color='grey',
+                    lh_color='grey',
+                    fig_size=(500, 500),
+                    azimuth=90,
+                    elevation=90):
     """Function for plotting triangular format Freesurfer surface of the brain.
+
+    Parameters
+    ----------
+    rh_vertices : :obj: `numpy.array`, optional
+        Array of right hemisphere vertex (x, y, z) coordinates, of size number_of_vertices x 3.
+        Default is None.
+    lh_vertices : :obj: `numpy.array`, optional
+        Array of left hemisphere vertex (x, y, z) coordinates, of size number_of_vertices x 3.
+        Default is None.
+    rh_faces : :obj: `numpy.array`, optional
+        Array defining right hemisphere mesh triangles, of size number_of_faces x 3.
+        Default is None.
+    lh_faces : :obj: `numpy.array`, optional
+        Array defining mesh triangles, of size number_of_faces x 3.
+        Default is None.
+    rh_color : :obj: `str`, :obj: `numpy.array`, optional
+        Color for each point/vertex/symbol of the right hemisphere, can be string format, 
+        examples for red:’red’, ‘#f00’, ‘#ff0000’ or ‘rgb(1,0,0), or rgb array of shape (N, 3).
+        Default value is 'grey'.
+    lh_color : :obj: `str`, :obj: `numpy.array`, optional
+        Color for each point/vertex/symbol of the left hemisphere, can be string format, 
+        examples for red:’red’, ‘#f00’, ‘#ff0000’ or ‘rgb(1,0,0), or rgb array of shape (N, 3).
+        Default value is 'grey'.
+    fig_size : (int, int), optional
+        Width and height of the figure. Default is (500, 500).
+    azimuth : int, optional
+        Angle of rotation about the z-axis (pointing up) in degrees. Default is 90.
+    elevation : int, optional
+        Vertical rotation where 90 means ‘up’, -90 means ‘down’, in degrees. Default is 90.
+
+    Returns
+    -------
+    :obj: `ipyvolume.Figure`
+        Ipyvolume object presenting the figure.
+    :obj: `ipyvolume.Mesh`
+        Ipyvolume object presenting the built mesh for right hemisphere.
+    :obj: `ipyvolume.Mesh`
+        Ipyvolume object presenting the built mesh for right hemisphere.
+
+    """
+
+    rh_mesh = None
+    lh_mesh = None
+
+    fig = ipv.figure(width=fig_size[0], height=fig_size[1], lighting=True)
+
+    if (rh_vertices is not None) and (rh_faces is not None):
+        rh_mesh = plot_hemisphere_mesh(rh_vertices, rh_faces, rh_color)
+    
+    if (lh_vertices is not None) and (lh_faces is not None):
+        lh_mesh = plot_hemisphere_mesh(lh_vertices, lh_faces, lh_color)
+
+    style.use('minimal')
+    ipv.view(azimuth, elevation)
+    ipv.squarelim()
+    ipv.show()
+
+    return fig, rh_mesh, lh_mesh
+
+
+def plot_hemisphere_mesh(vertices, faces,  color='grey'):
+    """Function for plotting triangular format Freesurfer surface of the brain hemispheres.
 
     Parameters
     ----------
