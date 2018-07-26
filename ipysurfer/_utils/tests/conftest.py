27b3d29a1de4bfbb284eec301bb579642577a12e
Oleh Kozynets <ok7mailbox@gmail.com>
2018-07-26 13:32:47 +0200

    Unit tests were added.

---
+++ ipysurfer/_utils/tests/conftest.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+import os.path as path
+
+import mne
+import pytest
+
+
+SubjectInfo = namedtuple('SubjectInfo', 'subject_id subjects_dir')
+
+
+@pytest.fixture(scope='module')
+def get_subject_info():
+    data_path = mne.datasets.sample.data_path()
+    subjects_dir = path.join(data_path, 'subjects')
+
+    return SubjectInfo(subject_id='sample', subjects_dir=subjects_dir)
